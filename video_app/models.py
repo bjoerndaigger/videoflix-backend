9f4b7e8041e1d8bf23e4e068f4b1bae9896e5efd
bjoerndaigger <bjoern.daigger@me.com>
2025-09-15 18:12:56 +0200

    feat: improve video model with categories and defaults

--- video_app/models.py
+++ video_app/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Video(models.Model):
+    class Category(models.TextChoices):
+        ENTERTAINMENT = "ent", "Entertainment"
+        EDUCATION = "edu", "Education"
+        NATURE = "nat", "Nature"
+        OTHER = "oth", "Other"
+
     created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField(max_length=255, default="Untitled")
+    description = models.TextField(default="No description")
     thumbnail_url = models.FileField(upload_to='thumbnails')
     video_file = models.FileField(upload_to='video')
-    category = models.CharField(max_length=100)
+
+    category = models.CharField(
+        max_length=20, choices=Category, default=Category.OTHER)
+
+    def __str__(self):
+        return self.title
